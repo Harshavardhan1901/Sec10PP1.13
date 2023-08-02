341fbaa28f0390b832c166429bce94d24359ae44
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-08-02 21:20:44 +0530

    first commit

---
+++ IPerson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section1._13
+{
+    public interface IPerson
+    {
+        string getInfo();
+        string getTypeOfPerson();
+    }
+}
